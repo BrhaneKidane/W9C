0403a11c80c5d3010ca824d7b53df7adee0d8307
Brhane <brhaneakidane@gmail.com>
2025-03-22 14:13:28 -0600

    added files

---
+++ app.js
@@ -0,0 +1,15 @@
+let item_names= ["Banana", "orange", "onion"]
+let item_prices = [20, 40, 60]
+let in_stock = [true, false, false]
+
+let first_item_name = item_names[0]
+let first_item_price = item_prices[0]
+
+item_names.push("cabbage")
+item_prices.push(80)
+in_stock.push(true)
+console.log(item_names)
+let last_item_name = item_names[3]
+let last_item_price = item_prices[3]
+let last_item_stock = in_stock[3]
+console.log(last_item_name, last_item_price, last_item_stock )
